df705c27802ba3e1d1a0aca94cadfc14ec9d295f
Sup3rPlays <73548496+SPFck@users.noreply.github.com>
2022-03-10 11:17:11 +0000

    Add files via upload

---
+++ SS_VADIR_F3_PSI/Main.Designer.cs
@@ -0,0 +1,277 @@
+﻿
+namespace SS_VADIR_F3_PSI
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monoFlat_ThemeContainer1 = new MonoFlat.MonoFlat_ThemeContainer();
+            this.btn_limpar = new MonoFlat.MonoFlat_Button();
+            this.btn_inserir = new MonoFlat.MonoFlat_Button();
+            this.monoFlat_Label5 = new MonoFlat.MonoFlat_Label();
+            this.monoFlat_Label4 = new MonoFlat.MonoFlat_Label();
+            this.monoFlat_Label3 = new MonoFlat.MonoFlat_Label();
+            this.monoFlat_Label2 = new MonoFlat.MonoFlat_Label();
+            this.monoFlat_Label1 = new MonoFlat.MonoFlat_Label();
+            this.txt_media = new MonoFlat.MonoFlat_TextBox();
+            this.txt_nota3 = new MonoFlat.MonoFlat_TextBox();
+            this.txt_nota2 = new MonoFlat.MonoFlat_TextBox();
+            this.txt_nota1 = new MonoFlat.MonoFlat_TextBox();
+            this.txt_nome = new MonoFlat.MonoFlat_TextBox();
+            this.monoFlat_ThemeContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // monoFlat_ThemeContainer1
+            // 
+            this.monoFlat_ThemeContainer1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(41)))), ((int)(((byte)(50)))));
+            this.monoFlat_ThemeContainer1.Controls.Add(this.btn_limpar);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.btn_inserir);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Label5);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Label4);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Label3);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Label2);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Label1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.txt_media);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.txt_nota3);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.txt_nota2);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.txt_nota1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.txt_nome);
+            this.monoFlat_ThemeContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.monoFlat_ThemeContainer1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.monoFlat_ThemeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.monoFlat_ThemeContainer1.Name = "monoFlat_ThemeContainer1";
+            this.monoFlat_ThemeContainer1.Padding = new System.Windows.Forms.Padding(10, 70, 10, 9);
+            this.monoFlat_ThemeContainer1.RoundCorners = true;
+            this.monoFlat_ThemeContainer1.Sizable = true;
+            this.monoFlat_ThemeContainer1.Size = new System.Drawing.Size(411, 450);
+            this.monoFlat_ThemeContainer1.SmartBounds = true;
+            this.monoFlat_ThemeContainer1.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultLocation;
+            this.monoFlat_ThemeContainer1.TabIndex = 0;
+            this.monoFlat_ThemeContainer1.Text = "Sistema Alunos";
+            // 
+            // btn_limpar
+            // 
+            this.btn_limpar.BackColor = System.Drawing.Color.Transparent;
+            this.btn_limpar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_limpar.Image = null;
+            this.btn_limpar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_limpar.Location = new System.Drawing.Point(244, 351);
+            this.btn_limpar.Name = "btn_limpar";
+            this.btn_limpar.Size = new System.Drawing.Size(146, 41);
+            this.btn_limpar.TabIndex = 11;
+            this.btn_limpar.Text = "Limpar";
+            this.btn_limpar.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.btn_limpar.Click += new System.EventHandler(this.btn_limpar_Click);
+            // 
+            // btn_inserir
+            // 
+            this.btn_inserir.BackColor = System.Drawing.Color.Transparent;
+            this.btn_inserir.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_inserir.Image = null;
+            this.btn_inserir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_inserir.Location = new System.Drawing.Point(22, 351);
+            this.btn_inserir.Name = "btn_inserir";
+            this.btn_inserir.Size = new System.Drawing.Size(146, 41);
+            this.btn_inserir.TabIndex = 10;
+            this.btn_inserir.Text = "Inserir";
+            this.btn_inserir.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.btn_inserir.Click += new System.EventHandler(this.btn_inserir_Click);
+            // 
+            // monoFlat_Label5
+            // 
+            this.monoFlat_Label5.AutoSize = true;
+            this.monoFlat_Label5.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Label5.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.monoFlat_Label5.ForeColor = System.Drawing.Color.White;
+            this.monoFlat_Label5.Location = new System.Drawing.Point(20, 273);
+            this.monoFlat_Label5.Name = "monoFlat_Label5";
+            this.monoFlat_Label5.Size = new System.Drawing.Size(64, 20);
+            this.monoFlat_Label5.TabIndex = 9;
+            this.monoFlat_Label5.Text = "Média:";
+            // 
+            // monoFlat_Label4
+            // 
+            this.monoFlat_Label4.AutoSize = true;
+            this.monoFlat_Label4.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.monoFlat_Label4.ForeColor = System.Drawing.Color.White;
+            this.monoFlat_Label4.Location = new System.Drawing.Point(22, 226);
+            this.monoFlat_Label4.Name = "monoFlat_Label4";
+            this.monoFlat_Label4.Size = new System.Drawing.Size(65, 20);
+            this.monoFlat_Label4.TabIndex = 8;
+            this.monoFlat_Label4.Text = "Nota3:";
+            // 
+            // monoFlat_Label3
+            // 
+            this.monoFlat_Label3.AutoSize = true;
+            this.monoFlat_Label3.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.monoFlat_Label3.ForeColor = System.Drawing.Color.White;
+            this.monoFlat_Label3.Location = new System.Drawing.Point(22, 178);
+            this.monoFlat_Label3.Name = "monoFlat_Label3";
+            this.monoFlat_Label3.Size = new System.Drawing.Size(65, 20);
+            this.monoFlat_Label3.TabIndex = 7;
+            this.monoFlat_Label3.Text = "Nota2:";
+            // 
+            // monoFlat_Label2
+            // 
+            this.monoFlat_Label2.AutoSize = true;
+            this.monoFlat_Label2.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.monoFlat_Label2.ForeColor = System.Drawing.Color.White;
+            this.monoFlat_Label2.Location = new System.Drawing.Point(22, 131);
+            this.monoFlat_Label2.Name = "monoFlat_Label2";
+            this.monoFlat_Label2.Size = new System.Drawing.Size(65, 20);
+            this.monoFlat_Label2.TabIndex = 6;
+            this.monoFlat_Label2.Text = "Nota1:";
+            // 
+            // monoFlat_Label1
+            // 
+            this.monoFlat_Label1.AutoSize = true;
+            this.monoFlat_Label1.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.monoFlat_Label1.ForeColor = System.Drawing.Color.White;
+            this.monoFlat_Label1.Location = new System.Drawing.Point(22, 85);
+            this.monoFlat_Label1.Name = "monoFlat_Label1";
+            this.monoFlat_Label1.Size = new System.Drawing.Size(63, 20);
+            this.monoFlat_Label1.TabIndex = 5;
+            this.monoFlat_Label1.Text = "Nome:";
+            // 
+            // txt_media
+            // 
+            this.txt_media.BackColor = System.Drawing.Color.Transparent;
+            this.txt_media.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_media.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(183)))), ((int)(((byte)(191)))));
+            this.txt_media.Image = null;
+            this.txt_media.Location = new System.Drawing.Point(105, 261);
+            this.txt_media.MaxLength = 32767;
+            this.txt_media.Multiline = false;
+            this.txt_media.Name = "txt_media";
+            this.txt_media.ReadOnly = true;
+            this.txt_media.Size = new System.Drawing.Size(286, 41);
+            this.txt_media.TabIndex = 4;
+            this.txt_media.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txt_media.UseSystemPasswordChar = false;
+            // 
+            // txt_nota3
+            // 
+            this.txt_nota3.BackColor = System.Drawing.Color.Transparent;
+            this.txt_nota3.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_nota3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(183)))), ((int)(((byte)(191)))));
+            this.txt_nota3.Image = null;
+            this.txt_nota3.Location = new System.Drawing.Point(105, 214);
+            this.txt_nota3.MaxLength = 32767;
+            this.txt_nota3.Multiline = false;
+            this.txt_nota3.Name = "txt_nota3";
+            this.txt_nota3.ReadOnly = false;
+            this.txt_nota3.Size = new System.Drawing.Size(286, 41);
+            this.txt_nota3.TabIndex = 3;
+            this.txt_nota3.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txt_nota3.UseSystemPasswordChar = false;
+            // 
+            // txt_nota2
+            // 
+            this.txt_nota2.BackColor = System.Drawing.Color.Transparent;
+            this.txt_nota2.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_nota2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(183)))), ((int)(((byte)(191)))));
+            this.txt_nota2.Image = null;
+            this.txt_nota2.Location = new System.Drawing.Point(105, 167);
+            this.txt_nota2.MaxLength = 32767;
+            this.txt_nota2.Multiline = false;
+            this.txt_nota2.Name = "txt_nota2";
+            this.txt_nota2.ReadOnly = false;
+            this.txt_nota2.Size = new System.Drawing.Size(286, 41);
+            this.txt_nota2.TabIndex = 2;
+            this.txt_nota2.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txt_nota2.UseSystemPasswordChar = false;
+            // 
+            // txt_nota1
+            // 
+            this.txt_nota1.BackColor = System.Drawing.Color.Transparent;
+            this.txt_nota1.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_nota1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(183)))), ((int)(((byte)(191)))));
+            this.txt_nota1.Image = null;
+            this.txt_nota1.Location = new System.Drawing.Point(104, 120);
+            this.txt_nota1.MaxLength = 32767;
+            this.txt_nota1.Multiline = false;
+            this.txt_nota1.Name = "txt_nota1";
+            this.txt_nota1.ReadOnly = false;
+            this.txt_nota1.Size = new System.Drawing.Size(286, 41);
+            this.txt_nota1.TabIndex = 1;
+            this.txt_nota1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txt_nota1.UseSystemPasswordChar = false;
+            // 
+            // txt_nome
+            // 
+            this.txt_nome.BackColor = System.Drawing.Color.Transparent;
+            this.txt_nome.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_nome.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(183)))), ((int)(((byte)(191)))));
+            this.txt_nome.Image = null;
+            this.txt_nome.Location = new System.Drawing.Point(105, 73);
+            this.txt_nome.MaxLength = 32767;
+            this.txt_nome.Multiline = false;
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.ReadOnly = false;
+            this.txt_nome.Size = new System.Drawing.Size(286, 41);
+            this.txt_nome.TabIndex = 0;
+            this.txt_nome.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txt_nome.UseSystemPasswordChar = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 450);
+            this.Controls.Add(this.monoFlat_ThemeContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.Text = "Sistema Alunos";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.monoFlat_ThemeContainer1.ResumeLayout(false);
+            this.monoFlat_ThemeContainer1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MonoFlat.MonoFlat_ThemeContainer monoFlat_ThemeContainer1;
+        private MonoFlat.MonoFlat_Button btn_limpar;
+        private MonoFlat.MonoFlat_Button btn_inserir;
+        private MonoFlat.MonoFlat_Label monoFlat_Label5;
+        private MonoFlat.MonoFlat_Label monoFlat_Label4;
+        private MonoFlat.MonoFlat_Label monoFlat_Label3;
+        private MonoFlat.MonoFlat_Label monoFlat_Label2;
+        private MonoFlat.MonoFlat_Label monoFlat_Label1;
+        private MonoFlat.MonoFlat_TextBox txt_media;
+        private MonoFlat.MonoFlat_TextBox txt_nota3;
+        private MonoFlat.MonoFlat_TextBox txt_nota2;
+        private MonoFlat.MonoFlat_TextBox txt_nota1;
+        private MonoFlat.MonoFlat_TextBox txt_nome;
+    }
+}
+
---
+++ SS_VADIR_F3_PSI/Main.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SS_VADIR_F3_PSI
+{
+    public partial class Main : Form
+    {
+        string path = "./alunos.txt";
+        UTF8Encoding encoding = new UTF8Encoding(true);
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+        public static bool numberonly_check(string str)
+        {
+            return !string.IsNullOrEmpty(str) && str.All(char.IsDigit);
+        }
+
+        private void btn_inserir_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(path))
+            {
+                Stream ws1 = new FileStream(path, FileMode.Create);
+                byte[] bytes = encoding.GetBytes("Nome  Nota1   Nota2   Nota3   Média\n");
+                ws1.Write(bytes, 0, bytes.Length);
+                ws1.Close();
+                MessageBox.Show("O ficheiro foi criado com sucesso!");
+            }
+
+            if(txt_nota1.Text == "" || txt_nota2.Text == "" || txt_nota3.Text == "" || txt_nome.Text == "")
+            {
+                MessageBox.Show("Os campos não podem estar vazios!");
+                return;
+            }
+
+            if(numberonly_check(txt_nota1.Text) && numberonly_check(txt_nota2.Text) && numberonly_check(txt_nota3.Text))
+            {
+                if(new[] { Convert.ToInt32(txt_nota1.Text), Convert.ToInt32(txt_nota2.Text), Convert.ToInt32(txt_nota3.Text) }.All(x => x >= 0 && x <= 20))
+                {
+                    FileStream ws2 = new FileStream(path, FileMode.Append);
+                    txt_media.Text = $"{(Convert.ToInt32(txt_nota1.Text)+Convert.ToInt32(txt_nota2.Text)+ Convert.ToInt32(txt_nota3.Text))/3}";
+                    string x_str = $"{txt_nome.Text}    {txt_nota1.Text}    {txt_nota2.Text}    {txt_nota3.Text}    {(Convert.ToInt32(txt_nota1.Text) + Convert.ToInt32(txt_nota2.Text) + Convert.ToInt32(txt_nota3.Text)) / 3}\n";
+                    byte[] bytes = encoding.GetBytes(x_str);
+                    ws2.Write(bytes, 0, bytes.Length);
+                    ws2.Close();
+                    MessageBox.Show("O aluno foi adicionado com sucesso!");
+                } else
+                {
+                    MessageBox.Show("As notas precisam ser de 0 a 20.");
+                }
+            } else
+            {
+                MessageBox.Show("Apenas números inteiros podem ser inseridos nas notas!");
+            }
+        }
+
+        private void btn_limpar_Click(object sender, EventArgs e)
+        {
+            txt_media.Text = "";
+            txt_nome.Text = "";
+            txt_nota1.Text = "";
+            txt_nota2.Text = "";
+            txt_nota3.Text = "";
+        }
+    }
+}
